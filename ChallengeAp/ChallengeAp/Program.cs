832705c5a0f64dcbd5005060e799cac0ccf9a4ba
Mateusz Wilusz <mateusz.wilusz93@gmail.com>
2023-10-26 20:19:05 +0200

    zadanie domowe dzien 4

--- ChallengeAp/ChallengeAp/Program.cs
+++ ChallengeAp/ChallengeAp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//liczy calkowite;
+
+int myAge = 120;
+int newAge = myAge + 5;
+int myVar = int.MinValue;
+uint myVar2 = uint.MaxValue;
+ulong myVar3 = ulong.MaxValue;
+
+//liczby zmiennoprzecinkoe
+float myNumber = float.MaxValue;
+float myNumber2 = double.MaxValue;
+
+//zmienne tekstowe
+string name = "Mateusz";
+string surname = "Wilusz";
+string result = name + surname;
+Console.WriteLine (result);
+
+
+
