66b5bd0254972fa24bdfd0642f66c925d0cfb470
Mateusz Wilusz <mateusz.wilusz93@gmail.com>
2023-10-26 20:15:20 +0200

    dzien 4 zadanie

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿var number1 = 50;
+var number2 = 10;
+
+if (number1 < number2)
+{
+    Console.WriteLine ("jestem tutaj w linji 5");
+}
+else
+{
+    Console.WriteLine("jestem tutaj w lini 10");
+
+}
