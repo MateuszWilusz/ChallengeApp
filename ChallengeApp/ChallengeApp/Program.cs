dccd7994e5f9426f15c940071b5c171160d00f37
Mateusz Wilusz <mateusz.wilusz93@gmail.com>
2023-10-26 20:20:31 +0200

    zadanie domowe dzien 4

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Design;
+
+string name = "Ewa";
+int age = 33;
+var sex = "kobieta";
+
+if (sex != "kobieta.") ;
+
+if (age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna.");
+}
+else
+{
+    Console.WriteLine("Pełnoletni mężczyzna.");
+}
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33.");
+}
+else if (age > 30)
+{
+    Console.WriteLine("Kobieta powyżej lat 30.");
+}
+else if (age == 30)
+{
+    Console.WriteLine("Kobieta lat 30.");
+}
+else if (age < 18)
+{
+    Console.WriteLine("Kobieta poniżej lat 30.");
+}
+else
+{
+    Console.WriteLine("Niepełnoletnia kobieta.");
+}
